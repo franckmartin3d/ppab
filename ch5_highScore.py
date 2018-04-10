f29f00aede9e389d73dc1dbeb2c33f4b9bf92d92
franckmartin3d <franck.martin3d@gmail.com>
2018-04-11 01:01:39 +1000

    ch4 page 1293

---
+++ ch5_highScore.py
@@ -0,0 +1,27 @@
+#franck 11/04/2018
+# Demonstrate list methods
+
+
+#empty list
+score = []
+
+#initialize variable (make it something)
+choice = None
+
+#continues until the user enters 0.
+while choice !="0":
+    print(
+    """
+    High Scores
+    
+    0 - EXIT
+    1 - Show Scores
+    2 - Add a score
+    3 - Delete a score
+    4 - Sort Score
+    """
+    )
+
+    choice = input("Choice: ")
+    print()
+
