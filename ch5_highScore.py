b6bcc0add0f159161fe7209255e3ed3b13ad49c5
franckmartin3d <franck.martin3d@gmail.com>
2018-04-12 00:32:16 +1000

    ch5_highscore

--- ch5_highScore.py
+++ ch5_highScore.py
@@ -3,7 +3,7 @@
 
 
 #empty list
-score = []
+scores = []
 
 #initialize variable (make it something)
 choice = None
@@ -24,5 +24,34 @@ while choice !="0":
 
     choice = input("Choice: ")
     print()
+#exit
+    if choice == "0":
+        print("Goodbye!")
+     # list high-score
+    elif choice =="1":
+        print("Hight Score")
+        for score in scores:
+            print(score)
 
-#commit
+    #add a score
+    elif choice =="2":
+        score = int(input("whats score did you get?:"))
+        scores.append(score)
+
+    #remove a score
+    elif choice == "3":
+        score = int(input("Romove which score?:"))
+        if score in scores:
+            scores.remove(score)
+        else:
+            print(score, "isnt in the high score list.")
+
+    #Sort score
+    elif choice == "4":
+        scores.sort(reverse=True)
+
+    # Some unknow choice
+    else:
+        print("Sorry, but ", choice, "isn't a valid choice")
+
+input("\n\nPress the enter key to exit.")
